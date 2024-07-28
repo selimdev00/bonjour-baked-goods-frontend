781bee19e066891e16fdd650d16316bee8267627
Selim Ataballyev <selimdev00@gmail.com>
2024-07-28 13:26:17 +0500

    [slider]: logic & styles & fixes

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import products from "./data/products.json";
 
 import Product from "./js/Product.js";
 
+import Slider from "./js/Slider.js";
+
 for (const product of products) {
   new Product(product);
 }
+
+Slider(document.querySelector(".products__slider"));
