e2b1fb1b484e2189616cdd87995f0bceebb87220
Selim Ataballyev <selimdev00@gmail.com>
2024-07-29 11:58:57 +0500

    [header]: header logic

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-"use strict";
-
 import products from "./data/products.json";
 
 import Product from "./js/Product.js";
-
 import Slider from "./js/Slider.js";
 
+// render products
 for (const product of products) {
   new Product(product);
 }
 
+// slider logic
 Slider(document.querySelector(".products__slider"));
+
+// header logic
+const header = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
